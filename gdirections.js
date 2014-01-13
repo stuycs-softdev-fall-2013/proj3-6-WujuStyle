3af678fb4a71b97d92bca1f22bc62c17c1c54688
Joshua Wakefield <rivers64kirby@gmail.com>
2014-01-13 12:06:39 -0500

    hithere

---
+++ gdirections.js
@@ -0,0 +1,9 @@
+var a = "http://maps.googleapis.com/maps/api/directions/json?origin=" 
+var b = "&destination="
+var c = "&mode="
+var d = "OK&sensor=false"
+
+directions = function(start, end, modeoftravel) {
+    return a+start+b+end+c+modeoftravel+d
+}
+    
