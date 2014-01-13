0a50d9c70c18ddfde810bf92acd1e91e9994219b
Stanley Zheng <szheng@gmail.com>
2014-01-13 12:02:46 -0500

    updating neighborhoods.py

--- neighborhoods.py
+++ neighborhoods.py
@@ -1,7 +1,20 @@
 import urllib2
+from bs4 import BeautifulSoup
+
+def getNeighborhoods():
+    r = []
+
+    response = urllib2.urlopen("http://en.wikipedia.org/wiki/Neighborhoods_in_New_York_City").read()
+
+    soup = BeautifulSoup(response)
 
 
-def getNeighborhoods()
-response = urllib.urlopen("http://en.wikipedia.org/wiki/Neighborhoods_in_New_York_City")
-html = response.read()
-string = html.find(
+    for x in soup.find("table",attrs={"class":"wikitable"}).find_all("tr"):
+        a = x.find_all("td")
+        if len(a) > 4:
+            r.append(a[4].get_text())
+
+    return r
+
+if __name__ == "__main__":
+    print getNeighborhoods()
