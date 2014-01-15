8b82136a0c9d21f64d6fc76fde17e60646fb3d10
Jae Ahn <JaeAhn@Jae-Ahns-MacBook-Pro.local>
2014-01-15 12:29:19 -0500

    my edition of locator.py; view and make changes according to andrew's now can get directions

--- locator.py
+++ locator.py
@@ -2,8 +2,8 @@ import urllib2
 import json
 from pymongo import MongoClient
 
-def getCoordinates(a):
-    url = "http://maps.googleapis.com/maps/api/geocode/json?address=%s&sensor=false"%(a.replace(" ","+"))
+def getCoordinates(d):
+    url = "http://maps.googleapis.com/maps/api/geocode/json?address=%s&sensor=false"%(d.replace(" ","+"))
     
     request = urllib2.Request(url, headers={'User-Agent':"Firefox Browser"})
     result = urllib2.urlopen(url)
@@ -14,8 +14,26 @@ def getCoordinates(a):
         a["borough"] = g["address_components"][3]["long_name"]
         a["neighborhood"] = g["address_components"][2]["long_name"]
         a["lat"] = g["geometry"]["location"]["lat"]
-        a["long"] = g["geometry"]["location"]["lng"]
+        a["long"] = g["geometry"]["location"]["long"]
+    
     except:
         return -1;
 
     return a 
+
+def getDirections(e): 
+    url = "http://maps.googleapis.com/maps/api/directions/json?origin=%s&destination=%s&sensor=false"%(e.replace(" "," "))
+    
+    request = urllib2.Request(url, headers = {'User-Agent': "Firefox"})
+    result = urllib2.urlopen(url);
+    
+    r = {}
+    try: 
+        k = json.loads(result.read())["results"][0]
+
+        r["how_to_get_there"]=k["directions"][3]["routes"]
+
+    except:
+        return -1
+    
+    return r
