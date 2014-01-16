0ea3794dad5f561e845b0f746f80e7a14408c637
Joshua Wakefield <rivers64kirby@gmail.com>
2014-01-16 12:15:40 -0500

    Update gdirections.py

--- gdirections.py
+++ gdirections.py
@@ -1,9 +1,14 @@
+import urllib2
+import simplejson
+
 a = "http://maps.googleapis.com/maps/api/directions/json?origin=" 
 b = "&destination="
 c = "&mode="
 d = "OK&sensor=false"
 
 def getUrl(start, end, modeoftravel) {
-    return a+start+b+end+c+modeoftravel+d
+    url = a+start+b+end+c+modeoftravel+d
+    json = urllib2.urlopen(url).read()
+    return json
 }
     
