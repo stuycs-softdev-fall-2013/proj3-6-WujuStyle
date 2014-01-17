af3d28ecfc672a3b1122901b18afbaa2470e204d
Jae Ahn <JaeAhn@jae-ahns-mbp.78m475.nycboe.org>
2014-01-17 12:08:18 -0500

    Locator

--- locator.py
+++ locator.py
@@ -19,3 +19,20 @@ def getCoordinates(b):
         return -1;
 
     return a 
+
+def getDirections(e): 
+    url = "http://maps.googleapis.com/maps/api/directions/json?origin=%s&destination=%s&sensor=false"%(e.replace(" "," "))
+    
+    request = urllib2.Request(url, headers = {'User-Agent': "Firefox"})
+    result = urllib2.urlopen(url);
+    
+    r = {}
+    try: 
+        k = json.loads(result.read())["results"][0]
+
+        r["how_to_get_there"]=k["directions"][3]["routes"]
+
+    except:
+        return -1
+    
+    return r
