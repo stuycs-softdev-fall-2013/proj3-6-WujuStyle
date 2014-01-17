704f8e041e4d622df1f22fc984a8f8f0c5179fe9
Joshua Wakefield <rivers64kirby@gmail.com>
2014-01-17 12:13:34 -0500

    hithere

---
+++ gdirections.py
@@ -0,0 +1,13 @@
+import urllib2
+
+a = "http://maps.googleapis.com/maps/api/directions/json?origin=" 
+b = "&destination="
+c = "&mode="
+d = "OK&sensor=false"
+
+def getUrl(start, end, modeoftravel):
+    url = a+start+b+end+c+modeoftravel+d
+    json = urllib2.urlopen(url).read()
+    return json
+
+print getUrl("Chicago","New York City","Driving")
