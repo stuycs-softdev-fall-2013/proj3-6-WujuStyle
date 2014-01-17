75011d761dec9affc14e11141597ce3f5c47ba03
Joshua Wakefield <rivers64kirby@gmail.com>
2014-01-17 14:55:04 -0500

    yo

--- gdirections.py
+++ gdirections.py
@@ -1,13 +1,16 @@
-import urllib2
+from urllib2 import urlopen
+import json
 
 a = "http://maps.googleapis.com/maps/api/directions/json?origin=" 
 b = "&destination="
-c = "&mode="
+"""c = "&mode="""
 d = "OK&sensor=false"
 
-def getUrl(start, end, modeoftravel):
-    url = a+start+b+end+c+modeoftravel+d
-    json = urllib2.urlopen(url).read()
-    return json
+def getUrl(start, end):
+    url = a+start+b+end+d
+    jsonurl = urlopen(url)
+    text = json.loads(jsonurl.read())
+    return text
 
-print getUrl("Chicago","New York City","Driving")
+q =  getUrl("Chicago","New York City")
+print q
