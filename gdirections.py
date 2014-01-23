d9f66fb2e9763a8dc5e2dfa8c988e049337e0b97
Joshua Wakefield <rivers64kirby@gmail.com>
2014-01-22 19:55:04 -0500

    Updated to allow spaces, for usage instructions on how to do lat,lang see previous post but heres a hint just make a string and have them separated by commas

--- gdirections.py
+++ gdirections.py
@@ -11,6 +11,7 @@ def is_number(s):
         return False
 
 def getUrl(start, end):
+    start,end=start.replace(" ","+"),end.replace(" ","+")
     if is_number(start[:9]):
         a="http://maps.googleapis.com/maps/api/directions/json?origin=%s&destination=%s&sensor=true&departure_time=%s&mode=%s"
     else:
@@ -31,6 +32,7 @@ def getUrl(start, end):
     text = text['routes'][0]['legs'][-1]
     tt = text['duration']['value']
     dict={'driving_time':dt, 'driving_distance':dd, 'transit_time':tt}
-    print calendar.timegm(time.gmtime())
-    print dt,dd,tt
     return dict
+
+if __name__ == "__main__":
+    getUrl("345 Chambers street nyc","309 west 104th street nyc")
