25bcb0bac8918ae2883ee347ece8e0f9d9988c2b
Jae Ahn <jae3ahn@gmail.com>
2014-01-27 04:12:50 -0500

    Does this work check it pls

--- neighborhoods.py
+++ neighborhoods.py
@@ -1,26 +1,17 @@
-import urllib2
+import csv
 from bs4 import BeautifulSoup
 
 def getNeighborhoods():
-    r = []
 
-    response = urllib2.urlopen("http://en.wikipedia.org/wiki/Neighborhoods_in_New_York_City").read()
+    lines = open("New_York_City_Population_By_Neighborhood_Tabulation_Areas.csv").readlines()    
+    newlines = {}
+    
+    for line in lines: 
+        if (line(population) >= 10,000)
+    else 
+    newlines[l[0]].append(l[1:])
 
-    soup = BeautifulSoup(response)
-
-
-    for x in soup.find("table",attrs={"class":"wikitable"}).find_all("tr"):
-        a = x.find_all("td")
-        if len(a) > 4:
-            k = a[4].get_text()
-            for y in k.split(","):
-                if y[0] == " ":
-                    y = y[1:]
-                if y not in r:
-                    r.append(y)
-
-    r = sorted(r)
-    return r
+    return newlines
 
 
 if __name__ == "__main__":
